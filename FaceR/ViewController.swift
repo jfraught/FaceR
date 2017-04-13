5e99ee44c9a928e16fbc18fbf1747ada2da3729c
Jordan Fraughton <jordan.fraughoton@gmail.com>
2017-04-13 09:47:26 -0600

    Initial Commit

---
+++ FaceR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceR
+//
+//  Created by Jordan Fraughton on 4/13/17.
+//  Copyright © 2017 Jordan Fraughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
