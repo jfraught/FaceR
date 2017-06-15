1bddad61af1ec330c2977532d9d075cf24de57fb
Jordan Fraughton <jordan.fraughoton@gmail.com>
2017-06-15 15:49:55 -0600

    Updates to MVC format.

---
+++ FaceR/AlbumCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCell.swift
+//  FaceR
+//
+//  Created by Jordan Fraughton on 6/7/17.
+//  Copyright © 2017 Jordan Fraughton. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    
+}
