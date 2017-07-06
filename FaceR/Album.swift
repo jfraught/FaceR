7e10cfd376eed2115dbbad5a6d7f7de810ca12c9
Jordan Fraughton <jordan.fraughoton@gmail.com>
2017-07-06 15:27:14 -0600

    Added segues between view controllers.

---
+++ FaceR/Album.swift
@@ -0,0 +1,58 @@
+//
+//  Album.swift
+//  FaceR
+//
+//  Created by Jordan Fraughton on 6/15/17.
+//  Copyright © 2017 Jordan Fraughton. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+class Album {
+    
+    var firstImageArray: [UIImage] = []
+    var albumNameArrary: [String] = []
+    let imgManager = PHImageManager.default()
+    let fetchOptions = PHFetchOptions()
+    
+    func fetchFirstImageFromUserCollections() {
+        
+        // Get user collections
+        
+        let userCollections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: PHFetchOptions())
+        print(userCollections.count)
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        requestOptions.deliveryMode = .highQualityFormat
+        
+        // Loop through user collections and take the first image of each collection and append it to the array.
+        
+        if userCollections.count > 0 {
+            
+            for i in 0..<userCollections.count {
+                
+                let album = userCollections.object(at: i)
+                
+                guard let firstImageAsset = PHAsset.fetchAssets(in: album, options: PHFetchOptions()).lastObject else { return }
+                
+                guard let albumName = album.localizedTitle else { return }
+                albumNameArrary.append(albumName)
+                
+                // firstImage: UIImage
+                
+                imgManager.requestImage(for: firstImageAsset, targetSize: CGSize.init(width: 175, height: 175), contentMode: .default, options: requestOptions, resultHandler:
+                    {
+                        image, error in
+                        
+                        if let firstImage = image {
+                            self.firstImageArray.append(firstImage)
+                            print(self.firstImageArray.count)
+                        }
+                })
+                
+            }
+        }
+    }
+
+}
---
+++ FaceR/StartSlideshowViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StartSlideshowViewController.swift
+//  FaceR
+//
+//  Created by Jordan Fraughton on 6/19/17.
+//  Copyright © 2017 Jordan Fraughton. All rights reserved.
+//
+
+import UIKit
+
+class StartSlideshowViewController: UIViewController {
+    @IBOutlet weak var slideshowStartImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
